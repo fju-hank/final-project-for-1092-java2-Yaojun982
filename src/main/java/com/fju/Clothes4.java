a1523edaa152ac8e3f7eab45ba86e1d83eeafa74
User <paul881125@yahoo.com.tw>
2021-06-15 18:21:47 +0800

    clothes size

--- src/main/java/com/fju/Clothes4.java
+++ src/main/java/com/fju/Clothes4.java
@@ -1,4 +1,20 @@
 package com.fju;
 
-public class Clothes4 {
+public class Clothes4 extends Clothes{
+    public Clothes4() {
+        length = 112;
+        width = 104;
+        height = 185;
+    }
+
+    @Override
+    public String getName() {
+        return "Clothes XL";
+    }
+
+    @Override
+    public int getPrice() {
+        return 1000;
+    }
 }
+
--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -1,7 +1,36 @@
 package com.fju;
 
+import java.util.Scanner;
+
+//////這是衣服搜尋男生的尺寸給你的size系統，這件衣服的價錢，輸入胸圍、腰圍、身高會告訴你是S、M、L、XL的size和價位
+
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Clothes[] clothes = {new Clothes1(), new Clothes2(),
+                new Clothes3(), new Clothes4()};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter object\'s bust: ");
+        int length = Integer.parseInt(scanner.next());
+        System.out.println("Please enter object\'s waist: ");
+        int width = Integer.parseInt(scanner.next());
+        System.out.println("Please enter object\'s height: ");
+        int height = Integer.parseInt(scanner.next());
+        System.out.println("Length: " + length + ", width: " + width + ", height: " + height);
+        for (Clothes box : clothes) {
+            if (box.validate(length, width, height)) {
+                System.out.println(box.getName() + " / " + box.getPrice());
+                break;
+            }
+        }
+        if (length <= 88 && width <= 76 && height <= 165) {
+            System.out.println("Clothes S it is, price: 100");      //1
+        } else if (length <= 96 && width <= 84 && height <= 175) {
+            System.out.println("Clothes M it is, price: 200");               //2
+        }else if (length <= 104 && width <= 94 && height <= 175) {
+            System.out.println("Clothes L it is, price: 300");            //3
+    }else if (length <= 112 && width <= 104 && height <= 185) {
+            System.out.println("Clothes XL it is, price: 1000");           //4
+        }
     }
 }
