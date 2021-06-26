70c9f63f181be09a318f72eb08c392f310c996ff
User <paul881125@yahoo.com.tw>
2021-06-26 14:52:30 +0800

    new gui added v2

---
+++ src/main/java/com/fju/SizeApp.java
@@ -0,0 +1,33 @@
+package com.fju;
+
+import javax.swing.*;
+
+public class SizeApp {
+    private JPanel JPanel0;
+    private JButton button1;
+    private JButton closeButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+
+    private JPanel JPanel2;
+    private JPanel JPanel3;
+    private JPanel JPanel4;
+    private JPanel JPanel5;
+    private JPanel JPanel6;
+    private JPanel JPanel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("SizeApp");
+        frame.setContentPane(new SizeApp().JPanel0);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+
+
+}
