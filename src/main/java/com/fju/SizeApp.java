e4e102ac72223908b4866a1d2b2b13e2fb8a939d
User <paul881125@yahoo.com.tw>
2021-06-26 17:50:12 +0800

    test6

--- src/main/java/com/fju/SizeApp.java
+++ src/main/java/com/fju/SizeApp.java
@@ -1,6 +1,8 @@
 package com.fju;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SizeApp {
     private JPanel JPanel0;
@@ -18,6 +20,29 @@ public class SizeApp {
     private JPanel JPanel5;
     private JPanel JPanel6;
     private JPanel JPanel1;
+
+    public SizeApp() {
+        button1.addActionListener(new ActionListener() {
+                Clothes[] clothes = {new Clothes1(), new Clothes2(),
+                        new Clothes3(), new Clothes4()};
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+            }
+        });
+        textField1.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SizeApp");
