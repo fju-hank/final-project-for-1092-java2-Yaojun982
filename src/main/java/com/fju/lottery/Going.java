7d989cba06f7658e4e23155afed01c80daf07459
User <paul881125@yahoo.com.tw>
2021-06-27 12:57:16 +0800

    lottery system added ing

---
+++ src/main/java/com/fju/lottery/Going.java
@@ -0,0 +1,11 @@
+package com.fju.lottery;
+
+    public class Going extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                System.out.println(getName() + " " +  i);
+            }
+        }
+    }
+
--- src/main/java/com/fju/lottery/LotteryClothes.java
+++ src/main/java/com/fju/lottery/LotteryClothes.java
@@ -1,4 +1,16 @@
 package com.fju.lottery;
 
 public class LotteryClothes {
+
+        public static void main(String[] args) {
+        Going A1 = new Going();
+        Going A2 = new Going();
+        LotteryRunnable A3 = new LotteryRunnable();
+        Thread thread = new Thread(A3);
+        thread.start();
+        A1.start();
+        A2.start();
+    }
+
+
 }
