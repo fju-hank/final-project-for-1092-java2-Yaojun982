92188e06fae14861496a5f39815800d4eac8ad61
User <paul881125@yahoo.com.tw>
2021-06-27 18:01:02 +0800

    Read file success

--- src/main/java/com/fju/read/ClothesRead.java
+++ src/main/java/com/fju/read/ClothesRead.java
@@ -1,18 +1,30 @@
 package com.fju.read;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
-
 public class ClothesRead {
     public static void main(String[] args) {
         try {
-            // Constructs a FileWriter object given a file name.
-            FileWriter fw = new FileWriter("衣服尺寸量法.txt");
-            fw.write("This is one!");
+            FileReader fr = new FileReader("衣服尺寸量法.txt");
+
+            BufferedReader br = new BufferedReader(fr);
+            String content = "";
+            while (br.ready()) {
+                content = br.readLine();
+                System.out.println("Ready... read txt");
+                System.out.println("-------------");
+                System.out.println(content);
+                System.out.println("-------------");
+            }
+
+            FileWriter fw = new FileWriter("target.txt");
+            fw.write(content);
             fw.flush();
+            System.out.println("success");
+            fr.close();
             fw.close();
         } catch (Exception e) {
-            System.out.println("Something Error");
+            System.out.println("failure");
         }
-
     }
 }
