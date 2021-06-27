aa0d5c8f1b70b742c80e4a32078f8aa26ec69527
User <paul881125@yahoo.com.tw>
2021-06-27 12:25:43 +0800

    gui message and lottery system added

--- src/main/java/com/fju/SizeApp.java
+++ src/main/java/com/fju/SizeApp.java
@@ -1,8 +1,11 @@
 package com.fju;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.text.NumberFormat;
 
 public class SizeApp {
     private JPanel JPanel0;
@@ -11,18 +14,25 @@ public class SizeApp {
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
 
     private JPanel JPanel2;
     private JPanel JPanel3;
     private JPanel JPanel4;
-    private JPanel JPanel5;
-    private JPanel JPanel6;
     private JPanel JPanel1;
 
 
+    public SizeApp() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object options[] = {"OK", "NO"};
+                JOptionPane.showOptionDialog(null, "您的尺寸大小" + "\n" + "胸圍 : " + textField1.getText() + "\n" + "腰圍 : " + textField2.getText() + "\n" + "身高 : " + textField3.getText() + "\n" + "Size and Money", "SizeApp", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+                JFormattedTextField intField = new JFormattedTextField(NumberFormat.getIntegerInstance());
+                intField.setFocusLostBehavior(JFormattedTextField.COMMIT);
 
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SizeApp");
@@ -33,6 +43,17 @@ public class SizeApp {
     }
 
 
+    public JComponent $$$getRootComponent$$$() {
 
+        return JPanel0;
+    }
 
+    private void createUIComponents() {
+
+    }
 }
+
+
+
+
+
---
+++ src/main/java/com/fju/lottery/LotteryClothes.java
@@ -0,0 +1,4 @@
+package com.fju.lottery;
+
+public class LotteryClothes {
+}
