d671f3a5b9592a83b3e25c0dce53b5e673bd2fa2
User <paul881125@yahoo.com.tw>
2021-06-27 13:01:03 +0800

    lottery system added ing v2

--- src/main/java/com/fju/lottery/LotteryClothes.java
+++ src/main/java/com/fju/lottery/LotteryClothes.java
@@ -5,11 +5,13 @@ public class LotteryClothes {
         public static void main(String[] args) {
         Going A1 = new Going();
         Going A2 = new Going();
-        LotteryRunnable A3 = new LotteryRunnable();
-        Thread thread = new Thread(A3);
+
+        LotteryRunnable A5 = new LotteryRunnable();
+        Thread thread = new Thread(A5);
         thread.start();
         A1.start();
         A2.start();
+        A5.run();
     }
 
 
